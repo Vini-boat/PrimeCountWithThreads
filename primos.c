88e285f8c8cbc2024a53eba69e81f8897ec051f2
Vinicius de Ávila Bezerra <85585157+Vini-boat@users.noreply.github.com>
2025-09-04 10:16:48 -0300

    Comments

--- primos.c
+++ primos.c
@@ -2,20 +2,32 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdbool.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 
-long n = 5000000;
-long chunk = 5000;
+
+long N = 5000000;
+long CHUNK = 5000;
+
+int benchmark_k[] = {1,2,4,6,8};
+int num_benchmarks = sizeof(benchmark_k) / sizeof(benchmark_k[0]);
+
+// Variáveis globais, serão acessadas via mutex
 long next = 0;
 long total = 0;
 
+// Criação de um mutex para cada variável
 pthread_mutex_t mutex_next;
 pthread_mutex_t mutex_total;
 
+
 bool is_prime(long num) {
     if (num <= 1) return false;
-    for (long i = 2; i * i <= num; i++) {
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    // i <= num / i para checar só até a raiz quadrada de num sem perder a precisão
+    // i+=2 para pular os pares maiores que 2
+    for (long i = 3; i <= num / i; i += 2) {
         if (num % i == 0) return false;
     }
     return true;
@@ -26,7 +38,7 @@ void print_progress_bar(int current, int n, int k) {
     int progress = (current * width) / n;
 	
     // Move o cursor para o início da linha
-    printf("\rk=%d [", k);
+    printf("\rk=%d\t [", k);
 
     for (int i = 0; i < width; i++) {
         if (i < progress) {
@@ -36,7 +48,7 @@ void print_progress_bar(int current, int n, int k) {
         }
     }
     printf("] %d%%", (current * 100) / n);
-    fflush(stdout); // força a atualização imediata
+    fflush(stdout); // força a atualização imediata para não piscar a tela
 }
 
 struct WorkArgs {
@@ -48,19 +60,18 @@ void *work(void *arg) {
 
     struct WorkArgs* args = (struct WorkArgs*)arg;
     int id = (int)args->id;
-    printf("Hello from thread %d!\n", id);
     while (true) {
         pthread_mutex_lock(&mutex_next);
 
         long start = next;
-        next += chunk;
+        next += CHUNK;
 
         pthread_mutex_unlock(&mutex_next);
+        // calcula só até o N
+        if (start >= N) break;
 
-        if (start >= n) break;
-
-        long end = start + chunk;
-        if (end > n) end = n;
+        long end = start + CHUNK;
+        if (end > N) end = N;
 
         long local_count = 0;
         for (long i = start; i < end; i++) {
@@ -70,7 +81,8 @@ void *work(void *arg) {
         }
         pthread_mutex_lock(&mutex_total);
 
-	    print_progress_bar(end, n,args->k);
+        // Printa a barra na região crítica para não ter concorrência pelo terminal
+	    print_progress_bar(end, N,args->k);
         total += local_count;
 
         pthread_mutex_unlock(&mutex_total);
@@ -92,6 +104,7 @@ struct BenchmarkResult benchmark(int num_threads) {
     pthread_mutex_init(&mutex_next, NULL);
     pthread_mutex_init(&mutex_total, NULL);
 
+    // Usa a clock_gettime para pegar o tempo real, não o tempo de cada thread
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
 
@@ -105,12 +118,16 @@ struct BenchmarkResult benchmark(int num_threads) {
     }
 
     clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // Converte pra milissegundos
     double time_spent = (end.tv_sec - start.tv_sec) * 1000.0;
     time_spent += (end.tv_nsec - start.tv_nsec) / 1000000.0;
 
     pthread_mutex_destroy(&mutex_next);
     pthread_mutex_destroy(&mutex_total);
 
+    printf("\n"); // para manter as barras de progresso dos outros k na tela
+    
     struct BenchmarkResult result = {num_threads, time_spent, total};
     return result;
 }
@@ -123,23 +140,23 @@ void print_header(){
 }
 
 void auto_benchmark(){
-    struct BenchmarkResult result[5];
-    result[0] = benchmark(1);
-    result[1] = benchmark(2);
-    result[2] = benchmark(4);
-    result[3] = benchmark(6);
-    result[4] = benchmark(8);
+    struct BenchmarkResult result[num_benchmarks];
+
+    for (int i = 0; i < num_benchmarks; i++) {
+        result[i] = benchmark(benchmark_k[i]);
+    }
 
     print_header();
-    for(int i=0;i<=4;i++){
-        double speedup = result[0].time_spent / result[i].time_spent;
-        printf("| %d\t| %.2f\t| %ld\t| %.2f\t\t|\n", result[i].num_threads, result[i].time_spent, result[i].total_primes, speedup);
+    for(int i=0;i<num_benchmarks;i++){
+        double speedup = result[0].time_spent / result[i].time_spent *100;
+        printf("| %d\t| %.2f\t| %ld\t| %.2f%%\t|\n", result[i].num_threads, result[i].time_spent, result[i].total_primes, speedup);
     }
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <number of threads>\n", argv[0]);
+        printf("Usage: %s -b\n", argv[0]);
         return 1;
     }
 
